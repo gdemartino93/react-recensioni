88eb64be318cce7d3b24d3499406260fee0f52d9
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-02-07 23:44:47 +0100

    create header and component title with style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import Title from "./components/Title";
 
 function App() {
   return (
     <div className="App">
-        
+        <header>
+          <Title name="Recensioni" />
+        </header>
+        <main>
+          
+        </main>
     </div>
   );
 }
