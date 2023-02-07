08c69e8e0091ae6b7e0969b7121a04d332a79b60
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-02-07 23:59:10 +0100

    create single review and style

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import { AiFillStar } from 'react-icons/ai'
 const Review = () => {
   return (
-    <div className='recensione'>Review</div>
+    <article className='recensione'>
+      <h4>Name</h4>
+      <p> loremloremloremloremloremloremloremloremloremlorememloremloremloremloremloremloremloremloremloremloremem</p>
+      <span><AiFillStar /></span>
+    </article>
   )
 }
 
