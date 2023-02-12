0ba1e03da3ff56162c517e772bbb593288931638
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-02-12 20:33:25 +0100

    d-block only to 1st slide

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { AiFillStar } from 'react-icons/ai'
-const Review = ({titolo,recensione,autore,voto}) => {
+const Review = ({titolo,recensione,autore,voto, classes}) => {
   return (
-    <article className='recensione'>
-      <h4>{autore}</h4>
+    <article className={`${classes}`}>
+      <h4 className=''>{autore}</h4>
       <p>{recensione}</p>
       <span><AiFillStar />{voto}</span>
       <span>{titolo}</span>
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,14 +1,20 @@
 import React, { useState } from 'react'
 import Review from './Review'
 import data from '../data';
+import { act } from 'react-dom/test-utils';
 const Slider = () => {
   const [recensioni , setRecensioni] = useState(data);
+  const [active , setActive] = useState(0);
   console.log(recensioni);
   return (
     <div className='slider'>
       {
-        recensioni.map(el => {
-          return  <Review key={el.id} {...el} />
+        recensioni.map((el,index) => {
+          let classActive = "d-none";
+          if ( active === index){
+            classActive = "d-block"
+          }
+          return  <Review key={el.id} {...el} classes={classActive} />
         })
       }
       {/* <Review titolo= "nome"  /> */}
